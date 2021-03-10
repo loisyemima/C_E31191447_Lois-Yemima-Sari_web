a287efae054b6b91a64967ae348bd6373d3f29bc
loisyemima <66865520+loisyemima@users.noreply.github.com>
2021-03-10 22:37:25 +0700

    Add files via upload

--- minggu2/praktikum.php
+++ minggu2/praktikum.php
@@ -1,1 +1,25 @@
+<?php
+class Car {
+    //properties
+    public $comp;
+    public $color = 'beige';
+    public $hasSunRoof = true;
 
+    //method = hello
+    public function hello(){
+        return "Beep I am a".$this ->comp.
+        "</i>, and I am". $this -> color;
+
+    }
+}
+
+//create object di class
+$bmw = new Car();
+$mercedes = new Car();
+
+$bmw -> color = 'blue';
+$bmw -> comp = "BMW";
+$mercedes -> comp = "Mercedes Benz";
+
+echo $bmw->hello();
+?>
