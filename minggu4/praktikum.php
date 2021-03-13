eaec31162fcae7f939a5b83c398148f8864b18f5
loisyemima <66865520+loisyemima@users.noreply.github.com>
2021-03-14 05:48:54 +0700

    Add files via upload

--- minggu4/praktikum.php
+++ minggu4/praktikum.php
@@ -1,1 +1,23 @@
+<?php
+class Car{
+    private $model;
+    
+    public function setModel($model){
+        $this->model=$model;
+    }
 
+    public function hello(){
+        return "beep! I am a <i>" . $this->model . "</i><br />";
+    }
+}
+
+class SportsCar extends Car{
+}
+
+$sportsCar1 = new SportsCar();
+
+$sportsCar1 -> setModel('Mercedes Benz');
+
+echo $sportsCar1->hello();
+
+?>
