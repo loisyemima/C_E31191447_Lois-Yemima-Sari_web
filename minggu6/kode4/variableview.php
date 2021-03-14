7eb91a95b7bb72b35d2f78accd33aa8cadabc655
loisyemima <66865520+loisyemima@users.noreply.github.com>
2021-03-15 00:19:38 +0700

    Add files via upload

---
+++ minggu6/kode4/variableview.php
@@ -0,0 +1,14 @@
+<html>
+<!--<head> Controller dan View</head>-->
+<head>
+    <title>Controller dan View lebih dari 1 Variabel</title>
+</head>
+<body>
+    <h2>Mengirim data dari controller ke View</h2>
+    
+    variabel1 : <?php echo variabel1; ?></br>
+    variabel1 : <?php echo variabel2; ?></br>
+
+
+</body>
+</html>
