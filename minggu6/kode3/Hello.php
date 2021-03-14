043d9d2134bd7f808dafa0d6da32bb788a38ba47
loisyemima <66865520+loisyemima@users.noreply.github.com>
2021-03-15 00:18:33 +0700

    Add files via upload

--- minggu6/kode3/Hello.php
+++ minggu6/kode3/Hello.php
@@ -1,1 +1,20 @@
+<?php
+class Hello extends CI_Controller{
+    public function index(){
+        //c3 - memuat model 'Hello_model'
+        $this->load->model('Hello_model');
+    
+        //penagmbilan objek dari kelas Hello_model dan dimuat di var $model
+        $model = $this->Hello_model;
 
+        //menagambil data dari model
+        $a = $model->txt;
+
+        //membuat data yang akan dikirim ke view
+        $data['teks'] = $a;
+
+        //memanggil file view
+        $this->load->view('helloview',$data);//file view
+    }
+}
+?>
---
+++ minggu6/kode3/helloview.php
@@ -0,0 +1,10 @@
+<html>
+<!--<head> Controller dan View</head>-->
+<head>
+    <title>Controller, Model dan View</title>
+</head>
+<body>
+    <h2><? echo $teks; ?></h2>
+    <h3>Menggunakan Controller dan View1</h3>
+</body>
+</html>
