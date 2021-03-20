7cb3818974da73cde49f35d100dbf52042d83704
loisyemima <66865520+loisyemima@users.noreply.github.com>
2021-03-20 08:52:06 +0700

    minggu7

---
+++ minggu7/ci/application/controllers/Demo_view2.php
@@ -0,0 +1,7 @@
+<?php
+class Demo_View2  extends CI_Controller{
+    public function index(){
+        $this->load->view('demoview');
+    }
+}
+?>
---
+++ minggu7/ci/application/views/contact.php
@@ -0,0 +1,7 @@
+<html>
+<head></head>
+<body>
+    <h2>Halaman Contact</h2>
+    <h3>Selamat datang ini adalah halaman Contact</h3>
+</body>
+</html>
